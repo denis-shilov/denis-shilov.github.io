bcfce987b83400b3259b607fd521388f5ac9161b
DenisShilov <kail000@yandex.ru>
2024-10-10 10:55:50 +0300

    Fixed some bugs and adjusted the navbar animation when scrolling

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
-// Изменяем стиль с примесью юмора :)
-window.addEventListener("scroll", () => {
-  document
-    .querySelector(".navbar")
-    .classList.toggle("scrolled", window.scrollY > 50);
-});
+window.onscroll = function () {
+  changeNavbarColor();
+  changeSizeLogo();
+};
+
+function changeNavbarColor() {
+  const navbar = document.getElementById("navbar");
+
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled"); // Добавляем класс при прокрутке
+  } else {
+    navbar.classList.remove("scrolled"); // Убираем класс, если вернулись наверх
+  }
+}
+
+function changeSizeLogo() {
+  const navbar = document.getElementById("logo");
+
+  if (window.scrollY > 50) {
+    logo.classList.add("logo_scrolled"); // Добавляем класс при прокрутке
+  } else {
+    logo.classList.remove("logo_scrolled"); // Убираем класс, если вернулись наверх
+  }
+}
